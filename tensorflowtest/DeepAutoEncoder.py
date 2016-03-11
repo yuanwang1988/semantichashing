543316fe514fc41f45e55f53615a2146c61548ab
yuanwang1988 <yuan.wang688@gmail.com>
2016-03-10 22:19:32 -0500

    small change to autoencoder

--- tensorflowtest/DeepAutoEncoder.py
+++ tensorflowtest/DeepAutoEncoder.py
@@ -72,7 +72,7 @@ def create(x, layer_sizes):
 def mnist_test():
 	sess = tf.Session()
 	x = tf.placeholder("float", [None, 784])
-	autoencoder = create(x, [392, 196, 98])
+	autoencoder = create(x, [784])
 	init = tf.initialize_all_variables()
 	sess.run(init)
 
