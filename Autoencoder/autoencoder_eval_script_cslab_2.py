6c03f2997bf03b647532eac554c429e0508f388b
Yuan Wang <yuan.wang688@gmail.com>
2016-04-21 09:58:15 -0400

    updated AE eval scripts

--- Autoencoder/autoencoder_eval_script_cslab_2.py
+++ Autoencoder/autoencoder_eval_script_cslab_2.py
@@ -1,6 +1,6 @@
 from autoencoder_eval_script import eval_autoencoder_hashlookup_precision_recall
 
-eval_autoencoder_hashlookup_precision_recall('MNIST_autoencoder_784_392_196_98_49_tanh', './results/final_models/MNIST_autoencoder_784_392_196_98_49_tanh_True_1', noise_flag = True, noise_level=1, Limit = 2500, visual_flag = False)
-eval_autoencoder_hashlookup_precision_recall('MNIST_autoencoder_784_392_196_98_49_tanh', './results/final_models/MNIST_autoencoder_784_392_196_98_49_tanh_True_2', noise_flag = True, noise_level=2, Limit = 2500, visual_flag = False)
-eval_autoencoder_hashlookup_precision_recall('MNIST_autoencoder_784_392_196_98_49_tanh', './results/final_models/MNIST_autoencoder_784_392_196_98_49_tanh_True_8', noise_flag = True, noise_level=8, Limit = 2500, visual_flag = False)
-eval_autoencoder_hashlookup_precision_recall('MNIST_autoencoder_784_392_196_98_49_tanh', './results/final_models/MNIST_autoencoder_784_392_196_98_49_tanh_True_16', noise_flag = True, noise_level=16, Limit = 2500, visual_flag = False)
+eval_autoencoder_hashlookup_precision_recall('MNIST_autoencoder_784_392_196_98_49_tanh', './working_models/MNIST_autoencoder_784_392_196_98_49_tanh_True_1', noise_flag = True, noise_level=1, Limit = 2500, visual_flag = False)
+eval_autoencoder_hashlookup_precision_recall('MNIST_autoencoder_784_392_196_98_49_tanh', './working_models/MNIST_autoencoder_784_392_196_98_49_tanh_True_2', noise_flag = True, noise_level=2, Limit = 2500, visual_flag = False)
+eval_autoencoder_hashlookup_precision_recall('MNIST_autoencoder_784_392_196_98_49_tanh', './working_models/MNIST_autoencoder_784_392_196_98_49_tanh_True_8', noise_flag = True, noise_level=8, Limit = 2500, visual_flag = False)
+eval_autoencoder_hashlookup_precision_recall('MNIST_autoencoder_784_392_196_98_49_tanh', './working_models/MNIST_autoencoder_784_392_196_98_49_tanh_True_16', noise_flag = True, noise_level=16, Limit = 2500, visual_flag = False)
